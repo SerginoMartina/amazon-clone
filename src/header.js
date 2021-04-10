a890f9d3bb929a4bbc05f8d8e5dc90b44921e5b6
sergino <sergino.martina@ida-mediafoundry.nl>
2021-04-10 20:15:02 +0200

    added files

---
+++ src/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function header() {
+  return (
+    <div>
+      <div className="header"></div>
+
+      <div className="header__search"></div>
+
+      <div className="header__nav"></div>
+    </div>
+  );
+}
+
+export default header;
